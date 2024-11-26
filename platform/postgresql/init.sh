bab44190e1bd4d1156ecfeda13361e23fa324770
msmenegol <marcelosmenegol@gmail.com>
2024-11-26 16:32:39 +0000

    Add dagster

--- platform/postgresql/init.sh
+++ platform/postgresql/init.sh
@@ -3,6 +3,13 @@
 psql -U ${POSTGRES_USER} <<-END
     create database ${MLFLOW_DB_USER};
     create user ${MLFLOW_DB_USER} with encrypted password '${MLFLOW_DB_PASSWORD}';
-    alter database mlflow owner to ${MLFLOW_DB_USER};
-    grant all privileges on database mlflow to ${MLFLOW_DB_USER};
+    alter database ${MLFLOW_DB_USER} owner to ${MLFLOW_DB_USER};
+    grant all privileges on database ${MLFLOW_DB_USER} to ${MLFLOW_DB_USER};
+END
+
+psql -U ${POSTGRES_USER} <<-END
+    create database ${DAGSTER_DB_USER};
+    create user ${DAGSTER_DB_USER} with encrypted password '${DAGSTER_DB_PASSWORD}';
+    alter database ${DAGSTER_DB_USER} owner to ${DAGSTER_DB_USER};
+    grant all privileges on database ${DAGSTER_DB_USER} to ${DAGSTER_DB_USER};
 END
