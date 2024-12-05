7fffcf6d774f5d92c56d73f252b0526a67e4df6f
msmenegol <marcelosmenegol@gmail.com>
2024-12-05 16:18:23 +0000

    Add airflow example dag

---
+++ platform/airflow/dags/dag_with_spark_and_deltalake_example.py
@@ -0,0 +1,126 @@
+import os
+import datetime as dt
+from random import seed, uniform
+
+from airflow.decorators import dag, task
+import boto3
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum, col
+
+
+@dag(start_date=dt.datetime(2024, 1, 1), schedule="@daily", catchup=False)
+def example_dag():
+    @task.pyspark(
+        conn_id="spark",
+    )
+    def setup_spark_session(spark, sc):
+        config_spark_session(spark)
+
+    @task.pyspark(conn_id="spark")
+    def create_data(spark, sc):
+        s3 = get_s3_client()
+        bucket_exists = True
+        try:
+            s3.get_bucket_acl(Bucket="example-data")
+        except:
+            bucket_exists = False
+        if not bucket_exists:
+            s3.create_bucket(Bucket="example-data")
+        spark = get_spark_session()
+        seed(42)
+        prices = spark.createDataFrame(
+            pd.DataFrame(
+                {
+                    "id": [i for i in range(100)],
+                    "date": pd.date_range(start="2024-01-01", periods=100),
+                    "price": [round(uniform(10, 2), 2) for _ in range(100)],
+                    "volume": [round(uniform(100, 5)) for _ in range(100)],
+                }
+            ).set_index("id")
+        )
+        prices.write.format("delta").mode("overwrite").save("s3a://example-data/prices")
+
+    @task.pyspark(conn_id="spark")
+    def create_monthly_volume(spark, sc):
+        prices = spark.read.format("delta").load("s3a://example-data/prices")
+        monthly_volumes = prices.groupBy(month("date").alias("month")).agg(
+            sum("volume").alias("volume")
+        )
+        monthly_volumes.write.format("delta").mode("overwrite").save(
+            "s3a://example-data/monthly_volumes"
+        )
+
+    @task.pyspark(conn_id="spark")
+    def create_yearly_revenue(spark, sc):
+        prices = spark.read.format("delta").load("s3a://example-data/prices")
+        prices = prices.withColumn("revenue", col("price") * col("volume"))
+        yearly_revenue = prices.groupBy(year("date").alias("year")).agg(
+            sum("revenue").alias("revenue")
+        )
+        yearly_revenue.write.format("delta").mode("overwrite").save(
+            "s3a://example-data/yearly_revenue"
+        )
+
+    @task.pyspark(conn_id="spark")
+    def create_monthly_price(spark, sc):
+        prices = spark.read.format("delta").load("s3a://example-data/prices")
+        prices = prices.withColumn("revenue", col("price") * col("volume"))
+        monthly_revenues = prices.groupBy(month("date").alias("month")).agg(
+            sum("revenue").alias("revenue")
+        )
+        monthly_volumes = spark.read.format("delta").load(
+            "s3a://example-data/monthly_volumes"
+        )
+        monthly_prices = monthly_revenues.join(monthly_volumes, on="month", how="left")
+        monthly_prices = monthly_prices.withColumn(
+            "price", col("revenue") / col("volume")
+        )
+        monthly_prices.write.format("delta").mode("overwrite").save(
+            "s3a://example-data/monthly_prices"
+        )
+
+    create_data_op = create_data()
+    setup_spark_session() >> create_data_op
+    create_data_op >> create_monthly_volume() >> create_monthly_price()
+    create_data_op >> create_yearly_revenue()
+
+
+def get_s3_client():
+    session = boto3.Session(
+        aws_access_key_id=os.environ.get("MINIO_ROOT_USER"),
+        aws_secret_access_key=os.environ.get("MINIO_ROOT_PASSWORD"),
+    )
+    s3 = session.client(
+        "s3",
+        endpoint_url="http://minio:9000",
+        config=boto3.session.Config(signature_version="s3v4"),
+    )
+    return s3
+
+
+def setup_spark_session(spark_session):
+    spark_session.conf.set("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
+    spark_session.conf.set(
+        "spark.hadoop.fs.s3a.access.key", os.environ.get("MINIO_ROOT_USER")
+    )
+    spark_session.conf.set(
+        "spark.hadoop.fs.s3a.secret.key", os.environ.get("MINIO_ROOT_PASSWORD")
+    )
+    spark_session.conf.set("spark.hadoop.fs.s3a.path.style.access", "true")
+    spark_session.conf.set(
+        "spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem"
+    ).spark_session.conf.set(
+        "spark.jars.packages",
+        "io.delta:delta-spark_2.12:3.2.0,org.apache.hadoop:hadoop-aws:3.3.4",
+    )
+    spark_session.conf.set(
+        "spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension"
+    )
+    spark_session.conf.set(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+
+
+example_dag()
